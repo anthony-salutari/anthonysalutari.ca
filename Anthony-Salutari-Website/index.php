17fe3da5e1688f878a75eac7c76635a6b5664493
Anthony Salutari <anthony17183@gmail.com>
2016-11-21 19:45:29 -0500

    Add basic coming soon page

---
+++ Anthony-Salutari-Website/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <link href="//netdna.boot<rapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://use.fontawesome.com/f3ca9e4607.js"></script>
+        
+        <title>Anthony Salutari</title>
+    </head>
+    <body>
+        
+        <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+        <h1>Coming Soon</h1>
+        <?php
+            
+        ?>
+        <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+    </body>
+</html>
